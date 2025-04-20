52c285dc8c9505fe0e0ac1ae74b2d89cdc1ad2ba
rubasace <ruben.pahino.verdugo@gmail.com>
2025-04-21 01:13:51 +0200

    hotfix: Fix build

--- src/main/java/dev/rubasace/linkedin/games_tracker/configuration/TelegramBotProperties.java
+++ src/main/java/dev/rubasace/linkedin/games_tracker/configuration/TelegramBotProperties.java
@@ -1,12 +1,22 @@
 package dev.rubasace.linkedin.games_tracker.configuration;
 
-import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
-@Data
+@Validated
+@Getter
 @ConfigurationProperties("dev.rubasace.linkedin.bot")
 public class TelegramBotProperties {
 
+    @NotBlank
     private final String username;
+    @NotBlank
     private final String token;
+
+    public TelegramBotProperties(final String username, final String token) {
+        this.username = username;
+        this.token = token;
+    }
 }
