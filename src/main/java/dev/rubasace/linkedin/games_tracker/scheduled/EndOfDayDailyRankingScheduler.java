fdf668d4298289d02cce6ae83e9b95c9b1fcc38c
rubasace <ruben.pahino.verdugo@gmail.com>
2025-04-23 23:03:07 +0200

    hotfix: Fix startup checks failing fue to the context not being ready

--- src/main/java/dev/rubasace/linkedin/games_tracker/scheduled/EndOfDayDailyRankingScheduler.java
+++ src/main/java/dev/rubasace/linkedin/games_tracker/scheduled/EndOfDayDailyRankingScheduler.java
@@ -3,21 +3,27 @@ package dev.rubasace.linkedin.games_tracker.scheduled;
 import dev.rubasace.linkedin.games_tracker.configuration.ExecutorsConfiguration;
 import dev.rubasace.linkedin.games_tracker.ranking.DailyRankingRecalculationService;
 import dev.rubasace.linkedin.games_tracker.util.LinkedinTimeUtils;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-class EndOfDayDailyRankingScheduler {
+class EndOfDayDailyRankingScheduler implements ApplicationListener<ApplicationReadyEvent> {
 
     private final DailyRankingRecalculationService dailyRankingRecalculationService;
 
     EndOfDayDailyRankingScheduler(final DailyRankingRecalculationService dailyRankingRecalculationService) {
         this.dailyRankingRecalculationService = dailyRankingRecalculationService;
-        this.dailyRankingRecalculationService.calculateMissingRankings();
     }
 
     @Scheduled(cron = "30 0 0 * * *", zone = LinkedinTimeUtils.LINKEDIN_ZONE, scheduler = ExecutorsConfiguration.SCHEDULED_TASKS_EXECUTOR_NAME)
     public void calculateMissingRankings() {
         dailyRankingRecalculationService.calculateMissingRankings();
+    }
+
+    @Override
+    public void onApplicationEvent(final ApplicationReadyEvent event) {
+        this.dailyRankingRecalculationService.calculateMissingRankings();
     }
 }
