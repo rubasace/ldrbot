37d55355b9f499984571e62cd33b7b07121ebec1
rubasace <ruben.pahino.verdugo@gmail.com>
2025-08-15 18:00:52 +0200

    hotfix: Fix GameRecord

--- vue/src/components/GameRecord.vue
+++ vue/src/components/GameRecord.vue
@@ -1,4 +1,5 @@
 <script setup>
+import BaseCard from './BaseCard.vue'
 
 defineProps({
   record: Object
@@ -58,7 +59,7 @@ defineProps({
     padding: 0.3em 0.75em
     background-color: #3c3c4b
     color: white
-    min-width: 55px
+    width: 55px
     font-size: 1.1em
     font-weight: bold
     border-radius: 999px
